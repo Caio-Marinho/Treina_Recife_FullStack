a5b565360417c2104b0048bc6ccda6c0eefd23f8
Caio-Marinho <kaigabriel12@gmail.com>
2025-01-07 10:50:33 -0300

    Exercicio do 18 até o 21 Treina Recife

--- Logica/Questao21.java
+++ Logica/Questao21.java
@@ -5,7 +5,7 @@ public class Questao21 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double area;
-        System.out.println("Digite a aréa do local a ser pintado em metros quadrados: ");
+        System.out.println("Informe a aréa do local a ser pintado em metros quadrados: ");
         area = scanner.nextDouble();
         double litros = area / 3;
         double latas = litros / 18;
