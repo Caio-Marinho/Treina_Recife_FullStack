f19e9f7c73166404261acf1f4392d4d400aac260
Caio-Marinho <kaigabriel12@gmail.com>
2025-01-08 14:46:47 -0300

    Correção da Questão 22

--- Logica/Questao22.java
+++ Logica/Questao22.java
@@ -1,27 +1,36 @@
 package Logica;
 import java.util.Scanner;
 
+
 public class Questao22 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double area;
         System.out.println("Informe a aréa do local a ser pintado em metros quadrados: ");
         area = scanner.nextDouble();
-        double litros = (area / 6) * 1.1;
-        int latas = (litros / 18) % 1 == 0 ? (int) litros / 18 : (int) (litros / 18) + 1;
-        double custo_latas = latas * 80;
-        int galao = (litros / 3.6) % 1 == 0 ? (int) (litros / 3.6) : (int) (litros / 3.6) + 1;
-        double custo_galao = galao * 25;
-        double mistura_lata_restante = litros % 18;
-        int mistura_galao = (int)(mistura_lata_restante / 3.6);
-        double custoMisturado = (latas * 80) + (mistura_galao * 25);
-        System.out.println("Quantidade de latas: " + latas);
-        System.out.println("Custo: " + custo_latas);
-        System.out.println("Quantidade de galões: " + galao);
-        System.out.println("Custo: " + custo_galao);
-        System.out.println("Quantidade de latas misturadas: " +  latas);
-        System.out.println("Quantidade de galões misturados: " + mistura_galao);
-        System.out.println("Custo: " + custoMisturado);
+        double areaComFolga = area * 1.1;
+        double litrosNecessarios = areaComFolga / 6;
+
+        int capacidadeLata = 18;
+        double precolata = 80.0;
+        double capacidadeGalao = 3.6;
+        double precoGalao = 25.0;
+        
+        int latasNecessarias = (int) Math.ceil(litrosNecessarios / capacidadeLata);
+        double custoLatas = litrosNecessarios * precolata;
+
+        int galoesNecessarios = (int) Math.ceil(litrosNecessarios / capacidadeGalao);
+        double custoGaloes = galoesNecessarios * precoGalao;
+
+        double restolitros = litrosNecessarios % capacidadeLata; 
+        int latasParaMistura = (int) (litrosNecessarios / capacidadeLata);
+        int galoesParaMistura = (int) Math.ceil(restolitros/capacidadeGalao);
+        double custoMistura = (latasParaMistura * precolata) + (galoesParaMistura * precoGalao);
+        
+        System.out.printf("Apenas Latas de 18 Litros: \n- Quatidade %d\n- Custo R$ %.2f%n",latasNecessarias, custoLatas);
+        System.out.printf("Apenas Galoes de 3.6 Litros: \n- Quatidade %d\n- Custo R$ %.2f%n",galoesNecessarios, custoGaloes);
+        System.out.printf("Latas de 18 Litros e Galoes de 3.6 Litros: \n- Latas %d\n- Galoes %d\n- Custo R$ %.2f%n",latasParaMistura,galoesParaMistura ,custoMistura);
+
         scanner.close();
     }
 }
