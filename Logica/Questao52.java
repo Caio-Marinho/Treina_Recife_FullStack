525bfe67e78da1d1da2fde79c13e946fcd5eb280
Caio-Marinho <kaigabriel12@gmail.com>
2025-01-10 15:36:31 -0300

    Atualização da 16

--- Logica/Questao52.java
+++ Logica/Questao52.java
@@ -3,25 +3,41 @@ import java.util.Scanner;
 public class Questao52 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int dia,mes,ano;
-        System.out.println("Digite o dia: ");
-        dia = sc.nextInt();
-        System.out.println("Digite o mês: ");
-        mes = sc.nextInt();
-        System.out.println("Digite o ano: ");
-        ano = sc.nextInt();
-        System.out.println("Data: "+dia+"/"+mes+"/"+ano);
-        sc.close();
-        if (mes == 2 && dia == 29 && (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0)) {
-            System.out.println("Data válida!");
-        } else if (mes == 2 && dia == 28 && !(ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0)) {
-            System.out.println("Data válida!");
-        } else if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12 && dia <= 31) {
-            System.out.println("Data válida!");
-        } else if (mes == 4 || mes == 6 || mes == 9 || mes == 11 && dia <= 30) {
-            System.out.println("Data válida!");
-        } else {
-            System.out.println("Data inválida!");
+        System.out.println("Informe a data no formato dd/mm/aaaa:");
+        String data = sc.nextLine();
+
+        String[] partes = data.split("/");
+        if (partes.length != 3) {
+            System.out.println("Data inválida,Use o formato dd/mm/aaaa.");
+            sc.close();
         }
+        
+        try {
+            int dia = Integer.parseInt(partes[0]);
+            int mes = Integer.parseInt(partes[1]);
+            int ano = Integer.parseInt(partes[2]);
+            
+            if (dataValida(dia, mes, ano)){
+                System.out.println("Data informada é válida");
+            } else {
+                System.out.println("Data informada é inválida");
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Data inválida,Use o formato dd/mm/aaaa.");
+        }
+    }
+    public static boolean dataValida(int dia, int mes, int ano) {
+        if (mes < 1 || mes > 12) {
+            return false;
+        }
+           int[] diasPorMes = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+           if (mes == 2 && anoBissexto(ano)) {
+               diasPorMes[1] = 29;
+           }
+           return dia > 0 && dia <= diasPorMes[mes - 1];
+    }
+
+    public static boolean anoBissexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0);
     }
 }
