8d007be7368815795448da3e6f798888405c6ffb
Caio-Marinho <kaigabriel12@gmail.com>
2025-01-13 16:46:58 -0300

    Atualização 79

--- Logica/Questao070LR.java
+++ Logica/Questao070LR.java
@@ -5,14 +5,15 @@ public class Questao070LR {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String nomeUsuario,senha;
-        System.out.println("Digite o nome do usuário: ");
-        nomeUsuario = sc.nextLine();
-        System.out.println("Digite a senha do usuário: ");
-        senha = sc.nextLine();
-        while (nomeUsuario.equals(senha)){ 
-            System.out.println("A senha não pode ser igual ao nome do usuário, digite novamente: ");
+        do{
+            System.out.println("Digite o nome do usuário: ");
+            nomeUsuario = sc.nextLine();
+            System.out.println("Digite a senha do usuário: ");
             senha = sc.nextLine();
-        }
+            if (nomeUsuario.equals(senha)){
+                System.out.println("Erro, o nome do usuário deve ser diferente da senha.");
+            }
+        } while (nomeUsuario.equals(senha));
         System.out.println("Nome do usuário: "+nomeUsuario);
         System.out.println("Senha: "+senha);
         sc.close();
