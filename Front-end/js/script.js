315391742843807b03e265523e89451a18e9e120
Caio-Marinho <kaigabriel12@gmail.com>
2025-01-20 21:15:17 -0300

    Travas posição da nuvens

--- Front-end/js/script.js
+++ Front-end/js/script.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -13,6 +14,7 @@ const jump = () => {
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    const cloudsPosition = clouds.offsetLeft;
     
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) { 
         pipe.style.animation = 'none';
@@ -24,6 +26,9 @@ const loop = setInterval(() => {
         mario.src = './images/game-over.png';
         mario.style.width = '150px';
         mario.style.marginLeft = '50px'; 
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
 
         clearInterval(loop);
     }
